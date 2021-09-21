be246788fc3c81426a3eec590c1c93757be514e7
Francesco Lombardi <f.lombardi79@gmail.com>
2021-09-21 11:38:12 +0200

    Aggiunto Bean CommandLineRunner all'applicazione

--- src/main/java/com/synclab/demoapp/gestionale/GestionaleApplication.java
+++ src/main/java/com/synclab/demoapp/gestionale/GestionaleApplication.java
@@ -1,13 +1,42 @@
 package com.synclab.demoapp.gestionale;
 
+import com.synclab.demoapp.gestionale.model.Role;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.synclab.demoapp.gestionale.repository.RoleRepository;
 
 @SpringBootApplication
 public class GestionaleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestionaleApplication.class, args);
+	}
+	
+	@Bean
+	CommandLineRunner init(RoleRepository roleRepository) {
+
+	    return args -> {
+
+	        Role adminRole = roleRepository.findByRole("ADMIN");
+	        if (adminRole == null) {
+	            Role newAdminRole = new Role();
+	            newAdminRole.setRole("ADMIN");
+	            roleRepository.save(newAdminRole);
+	        }
+
+	        Role userRole = roleRepository.findByRole("USER");
+	        if (userRole == null) {
+	            Role newUserRole = new Role();
+	            newUserRole.setRole("USER");
+	            roleRepository.save(newUserRole);
+	        }
+	    };
+
 	}
 
 }
